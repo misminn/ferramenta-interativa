017757c6bab84afaaa5e7940a14b77812dd655ac
michele <162150310+misminn@users.noreply.github.com>
2024-11-01 11:54:55 -0300

    s

---
+++ app.js
@@ -0,0 +1,7 @@
+function criaCartao (categoria,pergunta,respota) {
+    let container - document.getElementyById('container')
+    let cartao- document.createElement('article')
+    cartao.className - 'cartao'
+
+    
+
